fd7c9e0a80b3f453a8e3864b995a820c0c059b53
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-10-29 22:23:11 +0100

    update solution

--- factors.py
+++ factors.py
@@ -1,30 +1,30 @@
-def factorize_number(n):
-    factors = []
-    for i in range(2, n + 1):
-        while n % i == 0:
-            factors.append(i)
-            n //= i
-    return factors
+#!/usr/bin/python3
+"""factorize given set of numbers in a file to product of two small numbers"""
+from sys import argv
 
 
-def main(input_file, output_file):
-    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-        for line in infile:
-            n = int(line.strip())
-            if n > 1:
-                factors = factorize_number(n)
-                if len(factors) >= 2:
-                    p, q = factors[0], factors[1]
-                    outfile.write(f"{n}={p}*{q}\n")
+def factorize(numbers_to_check):
+    """"find two small numbers that multiply to give a given number"""
+    i = 2
+
+    if numbers_to_check < 2:
+        return
+
+    while numbers_to_check % i:
+        i += 1
+    print("{:.0f}={:.0f}*{:.0f}".format(numbers_to_check, numbers_to_check / i, i))
 
 
-if __name__ == "__main__":
-    import sys
+if len(argv) != 2:
+    exit()
 
-    if len(sys.argv) != 2:
-        print("Usage: factors <file>")
-        sys.exit(1)
+try:
+    with open(argv[1]) as file:
+        line = file.readline()
 
-    input_file = sys.argv[1]
-    output_file = "factorizations.txt"
-    main(input_file, output_file)
+        while line != "":
+            number_to_check = int(line.split('\n')[0])
+            factorize(number_to_check)
+            line = file.readline()
+except:
+    pass
